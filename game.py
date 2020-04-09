28bc54d8885b0781953cbccfaf5ace0aa5b73dbb
Sergey Ivanov <qbrick@mail.ru>
2020-04-09 12:05:37 +0300

    Application icon change

--- game.py
+++ game.py
@@ -5,13 +5,22 @@
 import sys
 
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import QSize
 
 from mainwindow import MainWindow
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    app.setStyle('Fusion')
 
-    navigator = MainWindow()
-    navigator.show()
+    app_icon = QIcon()
+    app_icon.addFile('./images/nut.png')
+    app_icon.addFile('./images/nut64x64.png', QSize(64,64))
+    app.setWindowIcon(app_icon)
+
+    mainwindow = MainWindow()
+    mainwindow.setWindowIcon(QIcon('./images/nut.png'))
+    mainwindow.show()
 
     sys.exit(app.exec_())
